5ad3a69eea285b2a13488084924793b154183fc4
Thuan Nguyen Phuc <thanhthaopl2011@gmail.com>
2026-03-21 16:22:01 +0700

    feat : add upload

--- upload.php
+++ upload.php
@@ -41,6 +41,6 @@
     <form method="post" action="" enctype="multipart/form-data">
         <input type="file" name="avatar"/>
         <input type="submit" name="uploadclick" value="Upload"/>
-    </form>
+    </form> 
 </body>
 </html>
